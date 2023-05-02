1fd98223e22df007256fa58b89ef9de2bea83766
claralm <92041756+claralm@users.noreply.github.com>
2023-05-02 21:57:43 +0200

    Commit

---
+++ Assets/RW/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject HayMachine;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - HayMachine.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = HayMachine.transform.position + offset;
+    }
+}
